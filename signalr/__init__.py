dc2e6b364a25843e5ff243bdf8343171d379d367
Stanislav Lazarov <s.a.lazarov@gmail.com>
2018-03-24 12:10:08 +0200

    Check for monkey patching.

--- signalr/__init__.py
+++ signalr/__init__.py
@@ -1,8 +1,11 @@
 from gevent import monkey
 
-monkey.patch_socket()
-monkey.patch_ssl()
+if monkey.is_module_patched('socket') is False:
+    monkey.patch_socket()
+
+if monkey.is_module_patched('ssl') is False:
+    monkey.patch_ssl()
 
 from ._connection import Connection
 
-__version__ = '0.0.7'
+__version__ = '0.0.7.2'
